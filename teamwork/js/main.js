d979d07f8c865581c8cd58a5d4548216369433b2
duaren <renaudduaren@gmail.com>
2021-04-26 16:06:17 +0000

    update twinmax js effect

--- teamwork/js/main.js
+++ teamwork/js/main.js
@@ -1,14 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
     window.addEventListener("load", function(e) {
         document.body.style.display = 'block';
 
         var tl = new TimelineMax();
-        tl.staggerFrom('h1', .5, {
+        tl.staggerFrom('section', 2.5, {
+        opacity: 0,
+        scale: .5,
+        ease: Power2.easeOut
+        }, 0.2) 
+
+        tl.staggerFrom('p, .links', 1, {
             opacity: 0,
             y: -40,
             ease: Power2.easeInOut
             }, 0.2, "-=2")
-        tl.staggerFrom('p', 1, {
+        tl.staggerFrom('.vector-image', 1.5, {
+            opacity: 0,
+            y: -40,
+            ease: Power2.easeInOut
+            }, 0.2, "-=1.5")
+        tl.staggerFrom('footer', 2, {
             opacity: 0,
             y: -40,
             ease: Power2.easeInOut
