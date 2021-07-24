c9690e88fd1f9024ebbc444ec119f836a46ef4a1
duaren <renaudduaren@gmail.com>
2021-07-24 02:15:22 +0100

    update default landing page

---
+++ main.js
@@ -0,0 +1,73 @@
+const psdList = [
+    {
+        id: 1,
+        name: "teamwork",
+        src: "./teamwork/",
+        img: "./teamwork/teamwork.jpg",
+        desc: "Team work simple landing page to share content and connect with peoples",
+    },
+    {
+        id: 2,
+        name: "gohub",
+        src: "./gohub/",
+        img: "./gohub/GoHub_Landing_Page.jpg",
+        desc: "Gohub multi section landing page",
+    },
+    {
+        id: 3,
+        name: "shelflife",
+        src: "./shelflife/",
+        img: "./shelflife/Shelflife_Sneaker_Store.png",
+        desc: "Sneakers store to promote sneaker brand and users community",
+    },
+    {
+        id: 4,
+        name: "photgraphy",
+        src: "./photgraphy/",
+        img: "./photgraphy/",
+        desc: "",
+    },
+    {
+        id: 5,
+        name: "learned",
+        src: "",
+        img: "",
+        desc: "",
+    },
+    {
+        id: 6,
+        name: "edumodo",
+        src: "",
+        img: "",
+        desc: "",
+    },
+    {
+        id: 7,
+        name: "fitness",
+        src: "",
+        img: "",
+        desc: "",
+    },
+    {
+        id: 8,
+        name: "shoeshop",
+        src: "",
+        img: "",
+        desc: "",
+    },
+    {
+        id: 9,
+        name: "acrostia",
+        src: "",
+        img: "",
+        desc: "",
+    },
+    {
+        id: 10,
+        name: "zet",
+        src: "",
+        img: "",
+        desc: "",
+    },
+];
+
